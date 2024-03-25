8839025b4cfb6a92be19947b8471ccb23c650d26
fdsafewa <37294298+fdsafewa@users.noreply.github.com>
2024-03-25 00:24:33 -0700

    update
    update the correct folder

--- OnlineWeb/Models/ProductImage.cs
+++
@@ -1,9 +0,0 @@
-public class ProductImage
-{
-    public int Id { get; set; } // Primary key
-    public string ImageUrl { get; set; }
-    public int ProductId { get; set; } // Foreign key to Product
-
-    // Navigation property back to the associated Product
-    public Product Product { get; set; }
-}
