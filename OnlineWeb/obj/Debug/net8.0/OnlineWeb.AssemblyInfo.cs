f0ec79b286fe94219eb7f3b902bd90f02004dac3
fdsafewa <37294298+fdsafewa@users.noreply.github.com>
2024-04-03 14:05:15 -0700

    some modify

--- OnlineWeb/obj/Debug/net8.0/OnlineWeb.AssemblyInfo.cs
+++ OnlineWeb/obj/Debug/net8.0/OnlineWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd42b9e40a979c998799c0c134ae3db47d377fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b945b0b4fd0c82d6cfddcc82c97c859cc81dc751")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
