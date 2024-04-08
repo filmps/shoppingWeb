4375ac526fd5876a76c82b35180ccde96ef3a197
hanaDSD <101930911+hanaDSD@users.noreply.github.com>
2024-04-08 10:45:28 -0700

    edit feedback controller

--- OnlineWeb/Controllers/FeedbackController.cs
+++ OnlineWeb/Controllers/FeedbackController.cs
@@ -6,12 +6,6 @@ using MailKit;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using MimeKit;
-using Microsoft.Extensions.Options;
-// using System.Net.Mail;
-using System.Net;
-using System;
-using System.Security.Authentication;
-
 
 namespace OnlineWeb.Controllers
 {
@@ -40,30 +34,14 @@ namespace OnlineWeb.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        // [HttpPost]
-        // public async Task<IActionResult> SendFeedback(Feedback feedback)
-        // {
-        //     if (ModelState.IsValid)
-        //     {
-        //         _context.Add(feedback);
-        //         await _context.SaveChangesAsync();
-        //         return RedirectToAction(nameof(Success));
-        //     }
-        //     return View(feedback);
-        // }
-
-
         [HttpPost]
         public async Task<IActionResult> SendFeedback(Feedback feedback)
 
         {
             var message = new MimeMessage();
-            // message.From.Add(new MailboxAddress("hanaliu2024", "hanaliu2024@gmail.com"));
             message.From.Add(new MailboxAddress("WebApp_Feedback", "519535143@qq.com"));
-            // message.To.Add(new MailboxAddress("Support", "xbwudi20092@gmail.com"));
             message.To.Add(new MailboxAddress("Support", "hanalau.unsw@gmail.com"));
             message.Subject = feedback.Title + "from: " + feedback.UserEmail;
-      
             message.Body = new TextPart("html") 
             {
                 Text = string.Format("<h1>{0}</h1><br><p>{1}</p>", feedback.Title, feedback.Content)
@@ -74,46 +52,12 @@ namespace OnlineWeb.Controllers
                 client.CheckCertificateRevocation = false;
 
                 client.ServerCertificateValidationCallback = (s, c, h, e) => true;
- 
-                // client.Connect("smtp.office365.com", 587,  false);
-
-                // client.Connect("smtp.gmail.com", 587, SecureSocketOptions.Auto);
-                // client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
                 client.Connect("smtp.qq.com", 587, SecureSocketOptions.StartTls);
-
-
-
-                // Note: only needed if the SMTP server requires authentication
-                // client.Authenticate("outlook_1A7BA034D7772E06@outlook.com", "eMbX!DJc67ac$u");
-                // client.Authenticate("hanaliu2024@gmail.com", "vnefvfwptovciubz");
                 client.Authenticate("519535143@qq.com", "pjlvpopquwhjbjdh");
-
-                // client.Send(message);
                 await client.SendAsync(message);
                 client.Disconnect(true);
                 return RedirectToAction(nameof(Success));
             }
-
-
-
-            // var mailMessage = new MailMessage
-            // {
-            //     From = new MailAddress("hanaliu2024@gmail.com", "SenderName"),
-            //     Subject = feedback.Title,
-            //     Body = feedback.Content,
-            //     IsBodyHtml = false
-            // };
-            // mailMessage.To.Add(new MailAddress("xbwudi20092@gmail.com"));
-
-            // using (var client = new SmtpClient("smtp.gmail.com", 465))
-            // {
-            //     client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            //     client.EnableSsl = true;
-            //     client.UseDefaultCredentials = false;// disable it
-            //     client.Credentials = new NetworkCredential("hanaliu2024@gmail.com", "vnefvfwptovciubz");
-            //     await client.SendMailAsync(mailMessage);
-            // }
-
             return RedirectToAction("Index");
         }
     }
