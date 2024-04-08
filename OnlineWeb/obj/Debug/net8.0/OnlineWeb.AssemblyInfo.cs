e656f4af6b481fa323d6d0170034bb147094d605
hanaDSD <101930911+hanaDSD@users.noreply.github.com>
2024-04-08 00:30:53 -0700

    edit send email

--- OnlineWeb/obj/Debug/net8.0/OnlineWeb.AssemblyInfo.cs
+++ OnlineWeb/obj/Debug/net8.0/OnlineWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca1ee26c1f1dc1cd33c32bcbdba4f106de2654d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643f357dfd49eeb17102360af9705935c92b4220")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
